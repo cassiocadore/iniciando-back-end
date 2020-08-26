8c53f8b44afde4ccbdff952824931391c32cf49c
Cássio Victor Cadore <cassiocadore@gmail.com>
2020-08-26 20:21:05 -0300

    Initial commit

---
+++ src/repositories/TransactionsRepository.ts
@@ -0,0 +1,70 @@
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  // [X] Done
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+  // [X] Done
+  public getBalance(): Balance {
+    const balance = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        accumulator.total = accumulator.income - accumulator.outcome;
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    return balance;
+  }
+
+  // [X] Done
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    // TODO
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
+  }
+
+  public hasBalance(value: number, type: string): boolean {
+    const totalBalance = this.getBalance();
+    if (type === 'outcome' && totalBalance.total < value) return false;
+    return true;
+  }
+}
+
+export default TransactionsRepository;
